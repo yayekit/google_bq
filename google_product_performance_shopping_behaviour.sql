95dbb9f27b629b872a5cd2af22018c98107b7915
Kit <yayekit@gmail.com>
2024-10-15 23:44:05 -0400

    refactored sql code for google bq

--- google_product_performance_shopping_behaviour.sql
+++ google_product_performance_shopping_behaviour.sql
@@ -1,33 +1,19 @@
-WITH product_actions AS (
-  SELECT
-    product.v2ProductName AS product_name,
-    fullVisitorId,
-    visitId,
-    SUM(CASE WHEN hits.eCommerceAction.action_type = '2' THEN 1 ELSE 0 END) AS product_views,
-    SUM(CASE WHEN hits.eCommerceAction.action_type = '3' THEN 1 ELSE 0 END) AS add_to_cart,
-    SUM(CASE WHEN hits.eCommerceAction.action_type = '6' THEN 1 ELSE 0 END) AS purchases
-  FROM
-    `bigquery-public-data.google_analytics_sample.ga_sessions_*`,
-    UNNEST(hits) AS hits,
-    UNNEST(hits.product) AS product
-  WHERE
-    _TABLE_SUFFIX BETWEEN '20170701' AND '20170731'
-    AND product.v2ProductName IS NOT NULL
-  GROUP BY
-    product_name, fullVisitorId, visitId
-)
-
 SELECT
-  product_name,
-  SUM(product_views) AS total_product_views,
-  SUM(add_to_cart) AS total_add_to_cart,
-  SUM(purchases) AS total_purchases,
-  COUNT(DISTINCT CONCAT(fullVisitorId, CAST(visitId AS STRING))) AS sessions,
-  COUNT(DISTINCT fullVisitorId) AS users
+  p.v2ProductName AS product_name,
+  COUNTIF(h.eCommerceAction.action_type = 2) AS total_product_views,
+  COUNTIF(h.eCommerceAction.action_type = 3) AS total_add_to_cart,
+  COUNTIF(h.eCommerceAction.action_type = 6) AS total_purchases,
+  COUNT(DISTINCT STRUCT(s.fullVisitorId, s.visitId)) AS sessions,
+  COUNT(DISTINCT s.fullVisitorId) AS users
 FROM
-  product_actions
+  `bigquery-public-data.google_analytics_sample.ga_sessions_*` AS s
+  CROSS JOIN UNNEST(s.hits) AS h
+  CROSS JOIN UNNEST(h.product) AS p
+WHERE
+  _TABLE_SUFFIX BETWEEN '20170701' AND '20170731'
+  AND p.v2ProductName IS NOT NULL
 GROUP BY
   product_name
 ORDER BY
   total_purchases DESC
-LIMIT 100
+LIMIT 100;
